97b18cdb47be1afe10b841e8396533bd0eac599c
Dino Tius <dinot@bizcover.com.au>
2017-09-11 05:49:07 +0000

    CP-4165 - Added global exception handling for BizCover.Utility.Document.Template.
    git-tfs-id: [https://bizcover.visualstudio.com/DefaultCollection]$/BizCoverMVC/Solution;C8019

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Reflection;
 using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using BizCover.Common.Infrastructure.Services;
+using NLog;
 
 namespace BizCover.Utility.Document.Template
 {
@@ -9,6 +10,8 @@ namespace BizCover.Utility.Document.Template
     {
         public static void Register(HttpConfiguration config)
         {
+            config.Services.Replace(typeof(IExceptionLogger), new NLogExceptionLogger(LogManager.GetLogger(Assembly.GetExecutingAssembly().GetName().Name)));
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
