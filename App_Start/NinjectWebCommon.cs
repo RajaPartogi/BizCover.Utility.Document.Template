a109a4191937ef31e38bad6618a5fdcdf92a4d79
mhomaei <36397015+mhomaei@users.noreply.github.com>
2018-04-16 18:34:06 -0700

    Upgrade Ninject (#1)

--- App_Start/NinjectWebCommon.cs
+++ App_Start/NinjectWebCommon.cs
@@ -1,68 +1,43 @@
+﻿using System;
+using System.Web;
 using BizCover.Common.Infrastructure.Logging;
-using BizCover.Utility.Document.Template.Services;
+using BizCover.Utility.Document.Template;
 using BizCover.Utility.Document.Template.Services.Classes;
 using BizCover.Utility.Document.Template.Services.Interfaces;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Ninject;
+using Ninject.Web.Common;
+using Ninject.Web.Common.WebHost;
 
-[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(BizCover.Utility.Document.Template.App_Start.NinjectWebCommon), "Start")]
-[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(BizCover.Utility.Document.Template.App_Start.NinjectWebCommon), "Stop")]
-
-namespace BizCover.Utility.Document.Template.App_Start
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
+namespace BizCover.Utility.Document.Template
 {
-    using System;
-    using System.Web;
-
-    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
-    using Ninject;
-    using Ninject.Web.Common;
-
-    public static class NinjectWebCommon 
+    public static class NinjectWebCommon
     {
-        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+        private static readonly Bootstrapper Bootstrapper = new Bootstrapper();
 
-        /// <summary>
-        /// Starts the application
-        /// </summary>
-        public static void Start() 
+        public static void Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
-            bootstrapper.Initialize(CreateKernel);
+            Bootstrapper.Initialize(CreateKernel);
         }
-        
-        /// <summary>
-        /// Stops the application.
-        /// </summary>
+
         public static void Stop()
         {
-            bootstrapper.ShutDown();
+            Bootstrapper.ShutDown();
         }
-        
-        /// <summary>
-        /// Creates the kernel that will manage your application.
-        /// </summary>
-        /// <returns>The created kernel.</returns>
+
         private static IKernel CreateKernel()
         {
             var kernel = new StandardKernel();
-            try
-            {
-                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
-                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
-                RegisterServices(kernel);
-                return kernel;
-            }
-            catch
-            {
-                kernel.Dispose();
-                throw;
-            }
-        }
+            kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+            kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
 
-        /// <summary>
-        /// Load your modules or register your services here!
-        /// </summary>
-        /// <param name="kernel">The kernel.</param>
+            RegisterServices(kernel);
+            return kernel;
+        }
         private static void RegisterServices(IKernel kernel)
         {
             kernel.Bind<IGenerateDocumentService>().To<GenerateDocumentService>().InTransientScope();
